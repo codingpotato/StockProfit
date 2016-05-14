7d1f7b706f53a050f6e58d97b4dfb1a3c819c4dd
parrot_F525B1E2D2 <parrot@cyber-dojo.org>
2016-05-14 14:29:33 +0000

    72

--- sandbox/StockProfit.tests.cpp
+++ sandbox/StockProfit.tests.cpp
@@ -176,6 +176,19 @@ TEST(StockProfit, Get_max_profits_for_two_day_prices) {
     ASSERT_EQ(expectMaxProfits, stockProfit.getMaxProfits());
 }
 
+TEST(StockProfit, Get_max_profits_after_buy_for_three_day_prices) {
+    int pricesValue[] = {1, 2, 3};
+    int pricesLength = sizeof(pricesValue) / sizeof(int);
+    vector<int> prices(pricesValue, pricesValue + pricesLength);
+    int maxProfitsAfterBuyValues[] = {-1, -2, -3};
+    vector<int> expectMaxProfitsAfterBuy(maxProfitsAfterBuyValues,
+        maxProfitsAfterBuyValues + pricesLength);
+
+    StockProfit stockProfit(prices);
+    ASSERT_EQ(expectMaxProfitsAfterBuy,
+        stockProfit.getMaxProfitsAfterBuy());
+}
+
 TEST(StockProfit, Get_max_profits_after_buy_for_four_day_prices) {
     int pricesValue[] = {1, 2, 3, 4};
     int pricesLength = sizeof(pricesValue) / sizeof(int);
