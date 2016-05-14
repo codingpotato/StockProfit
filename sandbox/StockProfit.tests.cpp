cbf4175bfa7d76c7cddab37edf1b8a1d95007f17
parrot_F525B1E2D2 <parrot@cyber-dojo.org>
2016-05-14 12:41:18 +0000

    13

--- sandbox/StockProfit.tests.cpp
+++ sandbox/StockProfit.tests.cpp
@@ -16,7 +16,7 @@ TEST(StockProfit, One_day_prices)
     int priceValues[] = {1};
     vector<int> prices(priceValues, priceValues + 1);
     vector<vector<int>> expectTransactions;
-    int transactionValues = {PASS};
+    int transactionValues[] = {PASS};
     expectTransactions.push_back(
         vector<int>(transactionValues, transactionValues + 1));
     
