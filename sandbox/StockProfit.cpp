6f9db84e7a6f4e79ee272b6040410823ba6ef9f8
parrot_F525B1E2D2 <parrot@cyber-dojo.org>
2016-05-14 12:33:30 +0000

    10

--- sandbox/StockProfit.cpp
+++ sandbox/StockProfit.cpp
@@ -6,5 +6,10 @@ StockProfit::StockProfit(vector<int> prices)
 
 vector<vector<int>> StockProfit::getTransactions() {
     vector<vector<int>> transactions;
+    if (prices.size() == 1) {
+        vector<int> transaction;
+        transaction.push_bakc(PASS);
+        transactions.push_back(transaction);
+    }
     return transactions;
 }
